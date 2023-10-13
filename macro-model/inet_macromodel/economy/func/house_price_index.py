debd408f1173d85be11d540fc0ff4798562bac0b
José Moran <jose.moran@polytechnique.org>
2023-10-13 14:56:37 +0100

    Fix autoreg not usable issue

--- macro-model/inet_macromodel/economy/func/house_price_index.py
+++ macro-model/inet_macromodel/economy/func/house_price_index.py
@@ -1,4 +1,5 @@
 import numpy as np
+import logging
 
 from abc import abstractmethod, ABC
 
@@ -16,26 +17,41 @@ class HPIForecasting(ABC):
 
 
 class HPIForecastingConstant(HPIForecasting):
-    def __init__(self, value: float):
+    def __init__(self, value: float, *args, **kwargs):
         self.forecaster = ConstantForecaster(value=value)
+        if args or kwargs:
+            logging.warning(
+                "HPIForecastingConstant: args and kwargs are not used. "
+                "Please check the documentation."
+            )
 
     def forecast_hpi_growth(self, historic_hpi_growth: np.ndarray) -> float:
         return self.forecaster.forecast(historic_hpi_growth)
 
 
 class HPIForecastingAutoReg(HPIForecasting):
-    def __init__(self, lags: int, window: int):
+    def __init__(self, lags: int, window: int, *args, **kwargs):
         self.forecaster = AutoregForecaster(lags)
         self.window = window
+        if args or kwargs:
+            logging.warning(
+                "HPIForecastingAutoReg: args and kwargs are not used. "
+                "Please check the documentation."
+            )
 
     def forecast_hpi_growth(self, historic_hpi_growth: np.ndarray) -> float:
         return self.forecaster.forecast(historic_hpi_growth[-self.window :])
 
 
 class HPIForecastingOLS(HPIForecasting):
-    def __init__(self, window: int):
+    def __init__(self, window: int, *args, **kwargs):
         self.forecaster = OLSForecaster()
         self.window = window
+        if args or kwargs:
+            logging.warning(
+                "HPIForecastingOLS: args and kwargs are not used. "
+                "Please check the documentation."
+            )
 
     def forecast_hpi_growth(self, historic_hpi_growth: np.ndarray) -> float:
         return self.forecaster.forecast(historic_hpi_growth[-self.window :])
