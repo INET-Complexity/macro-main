e00443388d5a155919378aa3ead9255585feca95
José Moran <jose.moran@polytechnique.org>
2023-10-18 18:38:42 +0200

    Exposed functions

--- macro-data/inet_data/__init__.py
+++ macro-data/inet_data/__init__.py
@@ -1,2 +1,4 @@
 from .creator import Creator
 from .run_creator import run_data
+from .util.check_existing_processed_data import check_existing_processed_data
+from .util.create_code import create_code
