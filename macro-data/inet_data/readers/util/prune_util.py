075c4488268f8506f1436c717b3afbfc08b22d2b
José Moran <jose.moran@polytechnique.org>
2023-12-06 17:30:43 +0100

    Pruning util

--- macro-data/inet_data/readers/util/prune_util.py
+++ macro-data/inet_data/readers/util/prune_util.py
@@ -9,25 +9,30 @@ class DataFilterWarning(Warning):
     pass
 
 
-def filter_columns_by_date(
-    columns: pd.Index, date: str | datetime | int, dataset_name: Optional[str] = None, date_format: str = "%Y-%m-%d"
+def prune_index(
+    index: pd.Index,
+    prune_date: str | datetime | int,
+    dataset_name: Optional[str] = None,
+    date_format: str = "%Y-%m-%d",
 ) -> list[str]:
     """
-    Returns
-     1. All the columns that cannot be parsed as a date,
-     2. The columns that can be parsed as a date and are greater than or equal to the date provided
+    Filters columns/index so datetime values are greater than or equal to the prune_date.
+
+    Returns a list with
+     1. All the values that cannot be parsed as a date,
+     2. The values that can be parsed as a date and are greater than or equal to the date provided
     """
 
     # if date is str, convert to datetime
-    if isinstance(date, str):
-        date = pd.to_datetime(date, format="%Y-%m-%d")
-    elif isinstance(date, int):
-        date = pd.to_datetime(str(date), format="%Y")
+    if isinstance(prune_date, str):
+        prune_date = pd.to_datetime(prune_date, format="%Y-%m-%d")
+    elif isinstance(prune_date, int):
+        prune_date = pd.to_datetime(str(prune_date), format="%Y")
 
     # identify indices of columns that can be parsed as a date
-    as_datetime = pd.to_datetime(columns, errors="coerce")
+    as_datetime = pd.to_datetime(index, errors="coerce")
 
-    non_date_cols = list(columns[as_datetime.isna()])
-    selected_date_cols = list(columns[as_datetime >= date])
+    non_date_cols = list(index[as_datetime.isna()])
+    selected_date_cols = list(index[as_datetime >= prune_date])
 
     return non_date_cols + selected_date_cols
