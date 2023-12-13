5eced2055a52a44078226169da6483288935ced2
José Moran <jose.moran@polytechnique.org>
2023-12-13 10:34:51 -0600

    synthetic gov entities, along with good functions to get exog data

--- macro-data/test_inet_data/unit/test_processing/synthetic_government_entities/test_synthetic_government_entities.py
+++ macro-data/test_inet_data/unit/test_processing/synthetic_government_entities/test_synthetic_government_entities.py
@@ -11,23 +11,13 @@ PARENT = pathlib.Path(__file__).parent.parent.parent.parent.resolve()
 
 
 class TestSyntheticGovernmentEntities:
-    def test__create(self, readers):
-        gov_entities = SyntheticDefaultGovernmentEntities(
+    def test__create(self, readers, all_exogenous_data, industry_data):
+        synth_gov_fra = SyntheticDefaultGovernmentEntities.create_from_readers(
+            readers=readers,
             country_name="FRA",
             year=2014,
+            exogenous_data=all_exogenous_data,
+            industry_data=industry_data,
+            single_government_entity=True,
         )
-        gov_entities.create(
-            single_government_entity=False,
-            monthly_govt_consumption_in_lcu=np.full(18, 3.0),
-            monthly_govt_consumption_in_usd=np.full(18, 3.5),
-            total_monthly_value_added_in_lcu=100.0,
-            total_number_of_firms=18,
-            total_gov_consumption_growth=np.array([0.04, 0.03]),
-        )
-
-        # Check if we have all the necessary fields
-        for gov_entities_field in ["Consumption in LCU", "Consumption in USD"]:
-            assert gov_entities_field in gov_entities.gov_entity_data.columns
-
-        # Check if there are any missing values
-        assert not np.any(pd.isna(gov_entities.gov_entity_data))
+        assert synth_gov_fra.number_of_entities == 1
