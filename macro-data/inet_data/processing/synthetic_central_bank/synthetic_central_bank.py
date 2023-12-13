c331ab9a2e9fe661159ec801564eb88159b3a4c6
José Moran <jose.moran@polytechnique.org>
2023-12-13 16:53:25 -0600

    better creator for central banks

--- macro-data/inet_data/processing/synthetic_central_bank/synthetic_central_bank.py
+++ macro-data/inet_data/processing/synthetic_central_bank/synthetic_central_bank.py
@@ -9,19 +9,10 @@ class SyntheticCentralBank(ABC):
         self,
         country_name: str,
         year: int,
+        central_bank_data: pd.DataFrame,
     ):
         self.country_name = country_name
         self.year = year
 
         # Bank data
-        self.central_bank_data = pd.DataFrame()
-
-    @abstractmethod
-    def create(self, initial_policy_rate: float) -> None:
-        pass
-
-    def create_agents(self, initial_policy_rate: float) -> None:
-        self.set_central_bank_policy_rate(initial_policy_rate)
-
-    def set_central_bank_policy_rate(self, initial_policy_rate: float) -> None:
-        self.central_bank_data["Policy Rate"] = [initial_policy_rate]
+        self.central_bank_data = central_bank_data
