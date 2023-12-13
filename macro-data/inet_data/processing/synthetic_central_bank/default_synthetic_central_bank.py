c0d7e1213f8a9eaac13ef853b36d3dfddefab2eb
José Moran <jose.moran@polytechnique.org>
2023-12-13 17:13:16 -0600

    cbanks

--- macro-data/inet_data/processing/synthetic_central_bank/default_synthetic_central_bank.py
+++ macro-data/inet_data/processing/synthetic_central_bank/default_synthetic_central_bank.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 
 from inet_data.processing.synthetic_central_bank.synthetic_central_bank import (
     SyntheticCentralBank,
 )
+from inet_data.readers.default_readers import DataReaders
 
 
 class SyntheticDefaultCentralBanks(SyntheticCentralBank):
@@ -10,11 +12,16 @@ class SyntheticDefaultCentralBanks(SyntheticCentralBank):
         self,
         country_name: str,
         year: int,
+        central_bank_data: pd.DataFrame,
     ):
         super().__init__(
             country_name,
             year,
+            central_bank_data,
         )
 
-    def create(self, initial_policy_rate: float) -> None:
-        self.create_agents(initial_policy_rate=initial_policy_rate)
+    @classmethod
+    def init_from_readers(cls, country_name: str, year: int, readers: DataReaders):
+        initial_policy_rate = readers.policy_rates.cb_policy_rate(country_name, year)
+        central_bank_data = pd.DataFrame({"Policy Rate": [initial_policy_rate]})
+        return cls(country_name, year, central_bank_data)
