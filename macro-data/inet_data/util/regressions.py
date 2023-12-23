3d83948c7176fa69c9d4ba1cfe0f758718a50b7b
José Moran <jose.moran@polytechnique.org>
2023-12-22 23:45:32 -0600

    modify model updates

--- macro-data/inet_data/util/regressions.py
+++ macro-data/inet_data/util/regressions.py
@@ -10,18 +10,19 @@ def fit_linear(
     household_data: pd.DataFrame,
     independents: list[str],
     dependent: str,
-) -> tuple[np.ndarray, Any]:
+    model: LinearRegression,
+) -> np.ndarray:
     dependent_data = household_data[dependent].values
     x = household_data[independents].values
     imp_mean = SimpleImputer(missing_values=np.nan, strategy="mean")
     x = imp_mean.fit_transform(x)
     if len(independents) == 0:
-        return dependent_data.mean(), None
+        return dependent_data.mean()
 
     # Fit
     # x = (x - x.min()) / (x.max() - x.min())
     x /= x.sum(axis=0)
-    reg = LinearRegression().fit(x, dependent_data)
-    pred = reg.predict(x)  # noqa
+    model.fit(x, dependent_data)
+    pred = model.predict(x)  # noqa
 
-    return pred, reg
+    return pred
