fe6a9e8de16e1ac9887e607e52ae1bebacb14291
José Moran <jose.moran@polytechnique.org>
2023-12-26 23:38:41 -0600

    changed way in which credit market is initialised (cleaner and easier to read)

--- macro-data/inet_data/processing/synthetic_credit_market/synthetic_credit_market.py
+++ macro-data/inet_data/processing/synthetic_credit_market/synthetic_credit_market.py
@@ -1,50 +1,10 @@
-from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
-import numpy as np
 import pandas as pd
 
 
-class SyntheticCreditMarket(ABC):
-    @abstractmethod
-    def __init__(
-        self,
-        country_name: str,
-        year: int,
-    ):
-        self.country_name = country_name
-        self.year = year
-
-        # Credit market data
-        self.credit_market_data = pd.DataFrame()
-
-    @abstractmethod
-    def create(
-        self,
-        bank_data: pd.DataFrame,
-        initial_firm_debt: np.ndarray,
-        initial_household_other_debt: np.ndarray,
-        initial_household_mortgage_debt: np.ndarray,
-        firms_corresponding_bank: np.ndarray,
-        households_corresponding_bank: np.ndarray,
-        initial_firm_loan_maturity: int,
-        household_consumption_loan_maturity: int,
-        mortgage_maturity: int,
-        assume_zero_initial_firm_debt: bool,
-    ) -> None:
-        pass
-
-    @abstractmethod
-    def set_initial_loans(
-        self,
-        bank_data: pd.DataFrame,
-        initial_firm_debt: np.ndarray,
-        initial_household_other_debt: np.ndarray,
-        initial_household_mortgage_debt: np.ndarray,
-        firms_corresponding_bank: np.ndarray,
-        households_corresponding_bank: np.ndarray,
-        initial_firm_loan_maturity: int,
-        household_consumption_loan_maturity: int,
-        mortgage_maturity: int,
-        assume_zero_initial_firm_debt: bool,
-    ) -> None:
-        pass
+@dataclass
+class SyntheticCreditMarket:
+    country_name: str
+    year: int
+    credit_market_data: pd.DataFrame
