b4927f3df5ff1648d3712e3c324c22fa2adb4281
José Moran <jose.moran@polytechnique.org>
2024-01-05 10:31:01 -0600

    Rename

--- macro-data/inet_data/__init__.py
+++ macro-data/inet_data/__init__.py
@@ -1,4 +1,4 @@
-from .creator import Creator
+from .data_wrapper import DataWrapper
 
 # from .run_creator import run_data
 from .util.check_existing_processed_data import check_existing_processed_data
