f266ac0c86f63d531c7e8311dc79a6fbd7aa4287
José Moran <jose.moran@polytechnique.org>
2024-01-31 18:28:52 +0100

    First release version

--- macro-data/setup.py
+++ macro-data/setup.py
@@ -13,7 +13,7 @@ with open(join(this_dir, "requirements.txt")) as f:
 
 setup(
     name="inet-macro-data",
-    version="0.1.0",
+    version="1.0.0",
     description="INET's MacroModel - Data",
     url="https://github.com/INET-Complexity/inet-macro-data",
     long_description_content_type="text/markdown",
