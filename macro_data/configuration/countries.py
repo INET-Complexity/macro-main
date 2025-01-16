c82c2ad0ad682c397c09453cb2a2a3d18c891553
José Moran <jose.moran@polytechnique.org>
2025-01-16 16:15:48 +0100

    isort

--- macro_data/configuration/countries.py
+++ macro_data/configuration/countries.py
@@ -1,7 +1,7 @@
-from strenum import StrEnum
 from pathlib import Path
 
 import yaml
+from strenum import StrEnum
 
 # get this file's directory path
 THIS_FILE_PATH = Path(__file__).parent.resolve()
