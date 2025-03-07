788a94bf3ff45454673bff959e71644370a00bd4
José Moran <jose.moran@polytechnique.org>
2025-03-07 11:51:22 +0100

    ROW docstrings

---
+++ macromodel/rest_of_the_world/func/__init__.py
@@ -0,0 +1,20 @@
+"""Rest of the World functions package.
+
+This package provides the core economic functions used by the Rest of the World
+component. Key functional areas include:
+
+1. Trade Functions:
+   - Import demand determination
+   - Export supply decisions
+   - Price setting mechanisms
+   - Excess demand management
+
+2. Price Functions:
+   - International price formation
+   - Currency conversion
+   - Price adjustment processes
+   - Inflation dynamics
+
+Each module implements specific economic behaviors that collectively
+determine the Rest of the World's interaction with modeled economies.
+"""
